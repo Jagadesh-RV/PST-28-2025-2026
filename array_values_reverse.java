adca4f3f8bfd7a93041e92c11eb6680c156549a8
Jagadesh-RV <155981328+Jagadesh-RV@users.noreply.github.com>
2026-01-20 09:40:32 +0530

    Add files via upload

---
+++ array_values_reverse.java
@@ -0,0 +1,8 @@
+public class array_values_reverse{
+	public static void main(String[] args) {
+	    int arr[]={1,2,3,4,5};
+	    for(int i=4;i>=0;i--){
+	        System.out.println(arr[i]);
+	    }
+	}
+}
