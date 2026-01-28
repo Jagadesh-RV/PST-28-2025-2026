67e8f93ead633a9e26d841e03fbc23b6c7ad10b3
Jagadesh-RV <155981328+Jagadesh-RV@users.noreply.github.com>
2026-01-28 10:58:50 +0530

    Add files via upload

---
+++ sum_of_array_elements.java
@@ -0,0 +1,10 @@
+public class sum_of_array_elements {
+    public static void main(String[] args) {
+        int[] arr={2,4,5,9,10};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+        }
+        System.out.println("The sum of array elements :"+sum);
+    }
+}
