44787985235da1bbc65d9d6ba6c819021f5dca4e
Jagadesh-RV <155981328+Jagadesh-RV@users.noreply.github.com>
2026-01-31 16:45:30 +0530

    Add files via upload

---
+++ ArrayByInput.java
@@ -0,0 +1,21 @@
+import java.util.Scanner; 
+public class ArrayByInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+        int[] array = new int[size];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            System.out.print("Element " + (i + 1) + ": ");
+            array[i] = scanner.nextInt(); 
+        }
+
+        System.out.println("\nArray elements:");
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        scanner.close(); 
+    }
+}
