de71f46f33e647f23c1def692f271925b572d161
Jagadesh-RV <155981328+Jagadesh-RV@users.noreply.github.com>
2026-02-01 16:49:46 +0530

    Add files via upload

---
+++ CharacterCount.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CharacterCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a string:");
+        String str = sc.nextLine();
+
+        System.out.println("Enter the character to count:");
+        char ch = sc.next().charAt(0);
+
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ch) {
+                count++;
+            }
+        }
+
+        System.out.println("Occurrences of '" + ch + "': " + count);
+    }
+}
