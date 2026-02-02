36b6ba333fab90641a85d98d8b0c1c4656f32e87
Jagadesh-RV <155981328+Jagadesh-RV@users.noreply.github.com>
2026-02-02 10:40:44 +0530

    Add files via upload

---
+++ largest_in_an_array.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class largest_in_an_array{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of the array: ");
+        int n = sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            System.out.println("Enter element "+(i+1) +" in an array:");
+            int num=sc.nextInt();
+            arr[i]=num;
+            
+        }
+        int len=arr.length;
+       
+        for(int i=0;i<len-1;i++){
+            int temp=arr[i];
+            if(arr[i]>arr[i+1]){
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
+            }
+        }
+        System.out.println("The largest element in the given array is :"+arr[len-1]);
+    }
+}
+
