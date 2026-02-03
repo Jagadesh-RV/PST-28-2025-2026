fd4c1ccc5775b2dbf844e19415ba170054512a53
Jagadesh-RV <155981328+Jagadesh-RV@users.noreply.github.com>
2026-02-03 14:16:25 +0530

    Update KaprekarNumber.java

--- KaprekarNumber.java
+++ KaprekarNumber.java
@@ -1,19 +1,4 @@
-import java.util.Scanner;
 
-public class KaprekarNumber {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int n = sc.nextInt();
-
-        int square = n*n ;
-        int digits = 0;
-
-        int temp = square;
-        while (temp != 0) {
-            digits++;
-            temp /= 10;
-        }
 import java.util.Scanner;
 
 public class KaprekarNumber {
@@ -41,13 +26,4 @@ public class KaprekarNumber {
     }
 }
 
-        temp = square;
-        left_part=temp%(digits/2);
-        right_part=temp/(digits/2);
-        if ((left_part+right_part)==n) {
-            System.out.println("The given number is a Kaprekar number");
-        } else {
-            System.out.println("The given number is not a Kaprekar number");
-        }
-    }
-}
+        
