635debc148b99c66ca07a69942a614c755f5ce6f
Jagadesh-RV <155981328+Jagadesh-RV@users.noreply.github.com>
2026-02-06 15:49:13 +0530

    Add introductory message for number printing
    Added a message before printing numbers from 0 to 100.

--- 1to100_loop_condition.java
+++ 1to100_loop_condition.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int i;
+        System.out.println("The numbers from 0 to 100 are :");
         for(i=0;i<=100;i++){
             System.out.println(i);
         }
@@ -8,3 +9,4 @@ public class Main {
     }
 
 }
+
