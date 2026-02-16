117a5db58360b6a717d660921cf8d9e6a6ffbfe2
Jagadesh R <155981328+Jagadesh-RV@users.noreply.github.com>
2026-02-16 23:02:48 +0530

    Add title message for character to string conversion
    Added a title message for character conversion.

--- ChartoString.java
+++ ChartoString.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 public class ChartoString {
     public static void main(String[] args) {
+        System.out.println("Character to String Conversion");
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a character:");
         char ch = sc.next().charAt(0);
         String result = Character.toString(ch);
         System.out.println("The string value is: " + result);
     }
+
 }
