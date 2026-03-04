807c2a1713dad732d4107d20819cd4495c78e4ea
Jagadesh R <155981328+Jagadesh-RV@users.noreply.github.com>
2026-03-04 14:54:58 +0530

    Update HCFofTwoNumber.java

--- HCFofTwoNumber.java
+++ HCFofTwoNumber.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class HCFofTwoNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter first number: ");
+        System.out.print("HCF of two numbers -Enter first number: ");
         int n1 = sc.nextInt();
         System.out.print("Enter second number: ");
         int n2 = sc.nextInt();
