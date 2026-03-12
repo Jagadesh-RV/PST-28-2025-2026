cd9f2d9a62c034361a30642fcadbc411d4e72540
Jagadesh R <155981328+Jagadesh-RV@users.noreply.github.com>
2026-03-12 22:05:20 +0530

    Updated

--- MostFrequentCharacter.java
+++ MostFrequentCharacter.java
@@ -4,9 +4,8 @@ public class MostFrequentCharacter {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter a string:");
+        System.out.println("To find most frequent character -Enter a string:");
         String str = sc.nextLine();
-
         int[] freq = new int[256];
 
         for (int i = 0; i < str.length(); i++) {
@@ -29,3 +28,4 @@ public class MostFrequentCharacter {
         System.out.println("Frequency: " + max);
     }
 }
+
