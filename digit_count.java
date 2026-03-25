561135bffb0a5717d8cab5560470b119be1458b6
Jagadesh R <155981328+Jagadesh-RV@users.noreply.github.com>
2026-03-25 23:19:15 +0530

    Updated

--- digit_count.java
+++ digit_count.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class digit_count{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.print("Digit Count-Enter a number: ");
         int n = sc.nextInt();
         int digit=0;
         while(n!=0){
