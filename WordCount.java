98135ca1dc58a6f64dd7d7780cd4897f71b830b8
Jagadesh R <155981328+Jagadesh-RV@users.noreply.github.com>
2026-03-29 19:48:08 +0530

    Updated

--- WordCount.java
+++ WordCount.java
@@ -4,7 +4,7 @@ public class WordCount {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter a string:");
+        System.out.println("Word count -Enter a string:");
         String str = sc.nextLine();
 
         int count = 0;
