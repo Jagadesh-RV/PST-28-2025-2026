ca1f9bd52bb01b03562e7c637a518014301b85bd
Jagadesh R <155981328+Jagadesh-RV@users.noreply.github.com>
2026-03-31 22:15:15 +0530

    Updated

--- StringtoLowercase.java
+++ StringtoLowercase.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class StringtoLowercase {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a string:");
+        System.out.println("Tring to lowercase-Enter a string:");
         String s = sc.nextLine();
 
         String upper = s.toLowerCase();
