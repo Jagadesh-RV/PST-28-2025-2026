ea9b4d3f543b6c25910d4fda721a4f54a21ccdb1
Jagadesh R <155981328+Jagadesh-RV@users.noreply.github.com>
2026-04-05 14:55:16 +0530

    Updated

--- StringtoCharacterArray.java
+++ StringtoCharacterArray.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class StringtoCharacterArray{
     public static void main(String[] args) {
          Scanner sc = new Scanner(System.in);
-         System.out.println("Enter a string:");
+         System.out.println("String to character -Enter a string:");
          String str = sc.nextLine();
          char[] charArray = str.toCharArray();
          System.out.println("The character array is:");
