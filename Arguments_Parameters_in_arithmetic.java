64454c77fcf8018ad2392e4a98577d82a02001df
Jagadesh R <155981328+Jagadesh-RV@users.noreply.github.com>
2026-07-15 22:40:06 +0530

    Updated

--- Arguments_Parameters_in_arithmetic.java
+++ Arguments_Parameters_in_arithmetic.java
@@ -21,7 +21,8 @@ public class Arguments_Parameters_in_arithmetic
 	public static void main(String[] args) {
 		int a=40;
 		int b=30;
-		System.out.println("Sum of "+ a +" and "+ b+" is :"+ sum(a,b));
+		System.out.println("Arithmetic operations:
+						   Sum of "+ a +" and "+ b+" is :"+ sum(a,b));
 		System.out.println("Average of "+ a +" and "+ b+" is :"+ average(a,b));
 		System.out.println("Dfference of "+ a +" and "+ b+" is :"+ difference(a,b));
 		System.out.println("Product of "+ a +" and "+ b+" is :"+ product(a,b));
